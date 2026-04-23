6dd45a0fc0de44a1f4b258a1f1b75be5244c377c
aayushesaini <aayushesaini1267@gmail.com>
2026-04-23 22:03:33 +0530

    Update outliers.py

--- Phase-2/outliers.py
+++ Phase-2/outliers.py
@@ -1,1 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+sns.boxplot(data['energy'])
+
+Q1 = data['energy'].quantile(0.25)
+Q3 = data['energy'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+data = data[(data['energy'] >= Q1 - 1.5*IQR) & (data['energy'] <= Q3 + 1.5*IQR)]

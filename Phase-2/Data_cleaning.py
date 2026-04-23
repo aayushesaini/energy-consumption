45a86aa8f121be5263d9dda8af4871a1ec45fca4
aayushesaini <aayushesaini1267@gmail.com>
2026-04-23 21:38:40 +0530

    Update Data_cleaning.py

--- Phase-2/Data_cleaning.py
+++ Phase-2/Data_cleaning.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+df = pd.read_excel("energy.xlsx")
+
+data = data.drop_duplicates()
+
+n = df.isnull().sum()
+
+if n<=50:
+    df = df.dropna()
+else:
+    df.fillna(df.mode().iloc[0], inplace=True)
